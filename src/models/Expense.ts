1f2bf51e640e858391f1f2b53992c90cae2b3736
Ankita7107 <ankita.gholap@technfest.com>
2026-05-14 12:15:29 +0530

    changes

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IExpense extends Document {
   date: string;
@@ -8,25 +8,36 @@ export interface IExpense extends Document {
   userId: string;
 }
 
-const ExpenseSchema: Schema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  date: { type: String, required: true },
-  category: { 
-    type: String, 
-    required: true,
-    enum: ['Room Rent', 'Travel to Home', 'Daily Travelling', 'Extra Travelling', 'Vegetables', 'Outside Food']
+const ExpenseSchema: Schema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    date: { type: String, required: true },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Room Rent",
+        "Travel to Home",
+        "Daily Travelling",
+        "Extra Travelling",
+        "Vegetables",
+        "Outside Food",
+      ],
+    },
+    amount: { type: Number, required: true },
+    notes: { type: String },
   },
-  amount: { type: Number, required: true },
-  notes: { type: String },
-}, {
-  timestamps: true,
-  toJSON: {
-    transform: (doc, ret: any) => {
-      ret.id = ret._id.toString();
-      delete ret._id;
-      delete ret.__v;
-    }
-  }
-});
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret: any) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  },
+);
 
-export default mongoose.models.Expense || mongoose.model<IExpense>('Expense', ExpenseSchema);
+export default mongoose.models.Expense ||
+  mongoose.model<IExpense>("Expense", ExpenseSchema);
